0d115db47bbd6f84c5f3488989fd1ef082887b74
ackermanjayjay <reza23198@gmail.com>
2023-06-21 21:58:31 +0700

    make cli library borrow book

--- bundle/main.js
+++ bundle/main.js
@@ -2,6 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const data_1 = require("./data");
 const dataKu = () => {
-    let listItems = data_1.Library.map((persons) => console.log(persons.id, persons.buku, persons.price));
+    console.log("\n==================Daftar buku=====================");
+    let listItems = data_1.Library.map((persons) => console.log("\n||", persons.id, "||", persons.buku, "||", persons.price));
+};
+const borrowBook = () => {
+    const pinjam = 7;
+    const pinjamBuku = "SDA";
+    let hasilSewa;
+    const denda = 100;
+    data_1.Library.map((book) => {
+        if (pinjam >= 7) {
+            console.log("\nlebih dari 7 hari");
+            console.log("Maka didenda 100 rupiah");
+            if (pinjamBuku == book.buku) {
+                hasilSewa = book.price;
+                console.log("Harga sewa: ", hasilSewa);
+                console.log("Denda: ", denda);
+                console.log("Total bayar anda + Denda: ", hasilSewa * denda);
+            }
+        }
+        else {
+            console.log("kurang dari 7");
+        }
+    });
 };
 dataKu();
+borrowBook();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
-import {Library} from './data'
+import { Library } from './data'
 
 const dataKu = () => {
-    let listItems = Library.map((persons: { id: any; buku: any; price: any })=>
-    console.log(persons.id,persons.buku,persons.price)
+    console.log("\n==================Daftar buku=====================")
+    let listItems = Library.map((persons: { id: any; buku: any; price: any }) =>
+        console.log("\n||",persons.id,"||", persons.buku,"||", persons.price)
     )
 }
+const borrowBook = () => {
+    const pinjam: number = 7
+    const pinjamBuku: string = "SDA"
+    let hasilSewa: number;
+    const denda: number = 100
+
+    Library.map((book: { id: number; buku: String; price: number }) => {
+        if (pinjam >= 7) {
+            console.log("\nlebih dari 7 hari")
+            console.log("Maka didenda 100 rupiah")
+            if (pinjamBuku == book.buku) {
+                hasilSewa = book.price
+                console.log("Harga sewa: ", hasilSewa)
+                console.log("Denda: ", denda)
+                console.log("Total bayar anda + Denda: ", hasilSewa * denda)
+            }
+        } else {
+            console.log("kurang dari 7")
+        }
+    })
+
+}
 dataKu()
+borrowBook()
